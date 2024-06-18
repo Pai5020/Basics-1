2b55947bc2c382cc0c28f68b093a92757ed7c0db
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-06-18 13:49:28 +0530

    new commit

--- lib/buttonListPage.dart
+++ lib/buttonListPage.dart
@@ -21,6 +21,40 @@ class ButtonListPage extends StatelessWidget {
     },
   ];
 
+  void navigateToPage(BuildContext context, String label) {
+    switch (label) {
+      case "Button1":
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => Button1(),
+          ),
+        );
+        break;
+
+      case 'Button2':
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => Button2()),
+        );
+        break;
+
+      case 'Button3':
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => Button3()),
+        );
+        break;
+
+      case 'Button4':
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => Button4()),
+        );
+        break;
+
+      case 'default':
+        print("Default!!!");
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,11 +66,73 @@ class ButtonListPage extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: MaterialButton(
               color: buttonData['color'],
-              onPressed: () {},
+              onPressed: () {
+                navigateToPage(context, buttonData['label']);
+              },
               child: Text(buttonData['label']),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class Button1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text("ButtonPAge1"),
+      ),
+      body: Center(
+        child: Text("Page 1"),
+      ),
+    );
+  }
+}
+
+class Button2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("ButtonPAge2"),
+      ),
+      body: Center(
+        child: Text("Page 2"),
+      ),
+    );
+  }
+}
+
+class Button3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("ButtonPAge3"),
+      ),
+      body: Center(
+        child: Text("Page 3"),
+      ),
+    );
+  }
+}
+
+class Button4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("ButtonPAge4"),
+      ),
+      body: Center(
+        child: Text("Page 4"),
       ),
     );
   }
