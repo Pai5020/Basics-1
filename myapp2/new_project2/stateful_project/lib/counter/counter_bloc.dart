bf7d6954865fc2909bed109d2714fc9c244f822f
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-06-22 11:39:34 +0530

    new commit

--- myapp2/new_project2/stateful_project/lib/counter/counter_bloc.dart
+++ myapp2/new_project2/stateful_project/lib/counter/counter_bloc.dart
@@ -5,9 +5,17 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
+  CounterBloc() : super(InitialState()) {
+    on<Increment>((event, emit) {
+      final currentstateValue = state.count;
+      final incrementedValue = currentstateValue + 1;
+      return emit(CounterState(count: incrementedValue));
+    });
+
+    on<Decrement>((event, emit) {
+      final currentstateValue = state.count;
+      final decrementedValue = currentstateValue - 1;
+      return emit(CounterState(count: decrementedValue));
     });
   }
 }
