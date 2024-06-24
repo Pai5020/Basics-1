1ebd7ed905e8e70193fcc82c77efe14e897b6535
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-06-25 00:22:01 +0530

    next

--- myapp2/new_project2/sample_app/lib/screens/login.dart
+++ myapp2/new_project2/sample_app/lib/screens/login.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ScreenLogin extends StatelessWidget {
-  const ScreenLogin({super.key});
+  ScreenLogin({super.key});
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +12,10 @@ class ScreenLogin extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextFormField(
+              controller: _usernameController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Username',
@@ -21,19 +25,63 @@ class ScreenLogin extends StatelessWidget {
               height: 20,
             ),
             TextFormField(
+              controller: _passwordController,
+              obscureText: true,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Password',
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
             ElevatedButton.icon(
-              onPressed: () {},
-              icon: Icon(Icons.check),
-              label: Text('Login'),
+              onPressed: () {
+                checkLogin(context);
+              },
+              icon: const Icon(Icons.check),
+              label: const Text('Login'),
             )
           ],
         ),
       ),
     ));
+  }
+
+  void checkLogin(BuildContext context) {
+    final _username = _usernameController.text;
+    final _password = _passwordController.text;
+
+    if (_username == _password) {
+      //goto home
+    } else {
+      final _errorMessage = 'Username password does not match';
+
+      //snackbar
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          behavior: SnackBarBehavior.floating,
+          margin: EdgeInsets.all(10),
+          content: Text(_errorMessage)));
+
+      //alert dialog
+
+      showDialog(
+          context: context,
+          builder: (context1) {
+            return AlertDialog(
+              title: Text('Error'),
+              content: Text(_errorMessage),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context1).pop();
+                    },
+                    child: Text('Close'))
+              ],
+            );
+          });
+
+      //Show Text
+    }
   }
 }
