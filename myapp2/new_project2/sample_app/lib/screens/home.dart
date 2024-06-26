ebd341032c04067ca603cc43a751fa81c4a7ea19
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-06-27 00:41:25 +0530

    new

--- myapp2/new_project2/sample_app/lib/screens/home.dart
+++ myapp2/new_project2/sample_app/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:sample_app/screens/login.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({super.key});
@@ -7,8 +8,26 @@ class ScreenHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('Home Screen'),
-    ));
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  signout(context);
+                },
+                icon: Icon(Icons.exit_to_app)),
+          ],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Text('Home Screen'),
+          ),
+        ));
+  }
+
+  signout(BuildContext context) {
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => ScreenLogin()),
+        (route) => false);
   }
 }
