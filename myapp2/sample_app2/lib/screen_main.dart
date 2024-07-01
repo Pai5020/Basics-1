6fa27dd9c2c5ebbe0b3d5e01c3b4cf37d3638245
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-07-01 21:22:55 +0530

    new

--- myapp2/sample_app2/lib/screen_main.dart
+++ myapp2/sample_app2/lib/screen_main.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ScreenMain extends StatelessWidget {
-  const ScreenMain({super.key});
+  ScreenMain({super.key});
+
+  final _list = ['Apple', 'Orange', 'Pine Apple'];
 
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Center(
-      child: Text('MAin screen'),
+      child: DropdownButtonFormField(
+          onChanged: (value) {
+            print(value);
+          },
+          items: _list.map((e) {
+            return DropdownMenuItem(value: e, child: Text(e));
+          }).toList()),
     ));
   }
 }
