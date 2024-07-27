82f599bdb0c7cbc33cc638b02b32a967ac4e3183
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-07-27 20:42:37 +0530

    first

---
+++ myapp2/animation_project/lib/animationDemo.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class AnimationDemo extends StatefulWidget {
+  @override
+  _AnimationDemoState createState() => _AnimationDemoState();
+}
+
+class _AnimationDemoState extends State<AnimationDemo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController
+      _controller; // Use 'late' keyword for non-nullable fields
+  late Animation<double>
+      _animation; // Use 'late' keyword for non-nullable fields
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 50, end: 200).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Animation'),
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Container(
+              width: _animation.value,
+              height: _animation.value,
+              color: Colors.blue,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
